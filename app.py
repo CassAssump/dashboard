0cc9406ee77a6bbdaf40d3d2f5f689f3da4f2c5e
Akageluv <akageluv@gmail.com>
2024-08-14 15:51:26 -0300

    MUDANÇA

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ import logging
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/bitcoin')
 def bitcoin():
     return render_template('btc.html')
@@ -23,20 +19,19 @@ def fetch_data():
     while True:
         try:
             btc_response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
-            btc_response.raise_for_status()  # Verifica se houve algum erro na requisição
+            btc_response.raise_for_status()
             btc_data = btc_response.json()
             
             eth_response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
             eth_data = eth_response.json()
 
-            # Emitir dados para as páginas específicas
             socketio.emit('updateBitcoin', {'btc_price': btc_data['bpi']['USD']['rate']})
             socketio.emit('updateEthereum', {'eth_price': eth_data['ethereum']['usd']})
         
         except requests.exceptions.RequestException as e:
             logging.error(f"Erro ao buscar dados: {e}")
 
-        time.sleep(5)  # Espera 5 segundos antes de buscar novos dados
+        time.sleep(5)
 
 @socketio.on('connect')
 def handle_connect():
